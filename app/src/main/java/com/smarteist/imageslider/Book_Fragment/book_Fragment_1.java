6f2e8d80639a2f02aec3c749f90b254e1741da80
gtogto <calicema59@gmail.com>
2020-11-13 10:09:33 +0900

    update

--- app/src/main/java/com/smarteist/imageslider/Book_Fragment/book_Fragment_1.java
+++ app/src/main/java/com/smarteist/imageslider/Book_Fragment/book_Fragment_1.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.smarteist.imageslider.R;
@@ -13,10 +14,13 @@ import androidx.fragment.app.Fragment;
 
 public class book_Fragment_1 extends Fragment {
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.book_fragment_1, container, false);
+
+        ImageView imageView = view.findViewById(R.id.imageView);
 
 
         return view;
