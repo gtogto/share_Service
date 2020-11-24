ccacdd198ff42017f6f3eca85808e6d7907d0a3e
gtogto <calicema59@gmail.com>
2020-11-24 12:39:41 +0900

    update by.gto

--- app/src/main/java/com/smarteist/shareOffice/Book_Fragment/reservationActivity.java
+++ app/src/main/java/com/smarteist/shareOffice/Book_Fragment/reservationActivity.java
@@ -1,22 +1,35 @@
 package com.smarteist.shareOffice.Book_Fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import com.smarteist.imageslider.R;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class reservationActivity extends AppCompatActivity {
-
+    private ImageView call_btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        Toolbar toolbar;
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        toolbar.setTitleTextColor(getResources().getColor(R.color.color_White));
-        getSupportActionBar().setTitle("Secure Access Lock");
         setContentView(R.layout.reservation);
+
+        call_btn = (ImageView)findViewById(R.id.call_btn);
+
+        call_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent mIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-9277-1592"));
+                startActivity(mIntent);
+
+            }
+        });
+
     }
 
 }
